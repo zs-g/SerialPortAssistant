680a5531d0f67d47bf5d2c34230117f6fcb590f1
温言 <zs-g@outlook.com>
2024-09-10 16:28:00 +0800

    使用  wpf 重构

---
+++ SerialPortAssistant/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SerialPortAssistant
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
