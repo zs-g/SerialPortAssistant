8e1c9a4f769c199f36417626efd5b96727a3750c
温言 <zs-g@outlook.com>
2024-09-14 15:41:18 +0800

    完成串口设置

--- SerialPortAssistant/ViewsModels/SerialPortViewModel.cs
+++ SerialPortAssistant/ViewsModels/SerialPortViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO.Ports;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace SerialPortAssistant.ViewsModels
 {
@@ -20,37 +21,77 @@ namespace SerialPortAssistant.ViewsModels
             _parity = (int)this._serialPort.Parity;
             // 默认串口名称
             _portName = this._serialPort.PortName;
+            // 是否打开串口
+            this.IsSerialPort = SerialPort.GetPortNames().Length > 0;
         }
 
         /// <summary>
         /// 波特率
         /// </summary>
-        [ObservableProperty]
         private int _baudRate;
+        public int BaudRate { 
+            get => _baudRate;
+            set
+            {
+                this._serialPort.BaudRate = value;
+                SetProperty(ref _baudRate, value);
+            }
+        }
 
         /// <summary>
         /// 数据位
         /// </summary>
-        [ObservableProperty]
         private int _dataBits;
+        public int  DataBits { 
+            get => _dataBits;
+            set
+            { 
+                this._serialPort.DataBits = value;
+                SetProperty(ref _dataBits, value);
+            }
+        }
 
         /// <summary>
         /// 停止位
         /// </summary>
-        [ObservableProperty]
         private int _stopBits;
+        public int StopBits
+        {
+            get => _stopBits;
+            set
+            {
+                this._serialPort.StopBits = (StopBits)value;
+                SetProperty(ref _stopBits, value);
+            }
+        }
 
         /// <summary>
         /// 校验位
         /// </summary>
-        [ObservableProperty]
         private int _parity;
+        public int Parity
+        {
+            get => _parity;
+            set
+            {
+                this._serialPort.Parity = (Parity)value;
+                SetProperty(ref _parity, value);
+            }
+        }
 
         /// <summary>
         /// 串口名称
         /// </summary>
-        [ObservableProperty]
         private string _portName;
+        public string PortName
+        {
+            get => _portName;
+            set
+            {
+                this._serialPort.PortName = value;
+                SetProperty(ref _portName, value);
+            }
+        }
 
         /// <summary>
         /// COM口列表
@@ -76,5 +117,29 @@ namespace SerialPortAssistant.ViewsModels
         /// 停止位列表
         /// </summary>
         public IEnumerable<int> StopBitsList { get; } = Enum.GetValues<StopBits>().Select(s => (int)s);
+
+        /// <summary>
+        /// 是否打开串口
+        /// </summary>
+        [ObservableProperty]
+        [NotifyCanExecuteChangedFor(nameof(SwitchCommand))]
+        private bool _isSerialPort;
+
+        /// <summary>
+        /// 串口开关
+        /// </summary>
+        [RelayCommand(CanExecute = nameof(IsSerialPort))]
+        public void Switch()
+        { 
+            if (this._serialPort.IsOpen)
+            {
+                this._serialPort.Close();
+            }
+            else
+            {
+                this._serialPort.Open();
+            }
+        }
+        
     }
 }
