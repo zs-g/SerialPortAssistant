dbd3e251ab4a9a0eca4a5f117e83770cc1ae5f20
温言 <zs-g@outlook.com>
2024-09-14 13:17:00 +0800

    完成界面布局

--- SerialPortAssistant/Views/SerialPort.xaml.cs
+++ SerialPortAssistant/Views/SerialPort.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SerialPortAssistant
+namespace SerialPortAssistant.Views
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// SerialPort.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SerialPort : Window
     {
-        public MainWindow()
+        public SerialPort()
         {
             InitializeComponent();
         }
